8a61af56cdab7a805d604ea5e839d84650b3cd1b
M-o-ha-med <120997421+M-o-ha-med@users.noreply.github.com>
2025-09-08 23:02:17 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,20 +8,12 @@ from sklearn.metrics import mean_squared_error, r2_score
 import mlflow
 from joblib import dump
 
-# Set MLflow Tracking URI
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
- 
- 
-# Create a new MLflow Experiment
-mlflow.set_experiment("Online Training House price prediction")
-
 dataset = sys.argv[1] if len(sys.argv) == 1 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "clean dataset.csv")
 df = pd.read_csv(dataset)
 
 LRModel = LinearRegression()
 
 with mlflow.start_run():
-    mlflow.autolog()
 
     X = df.drop(['price'],axis=1)
     y = df['price']
@@ -38,14 +30,7 @@ with mlflow.start_run():
     print("Linear Regression Model Evaluation:")
     print("Mean Squared Error:", mean_squared_error(y_test, result_3))
     print("R-squared:", r2_score(y_test, result_3))
-
-    # Simpan model ke file lokal
-    dump(LRModel, "online_model_LinearRegression.joblib")
-
-
-    mlflow.log_artifact("online_model_LinearRegression.joblib" , artifact_path="model_artifacts")
-
-
+ 
     mlflow.sklearn.log_model(
         sk_model = LRModel,
         artifact_path = "online_model_LinearRegression",
